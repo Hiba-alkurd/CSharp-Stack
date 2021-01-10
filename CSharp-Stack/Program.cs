74d05b0d3c98d972f9e60dfdbfa0d52a0ed7fb3a
Hiba Alkurd <hibaalkurd@gmail.com>
2021-01-10 17:01:41 +0200

    first commit

---
+++ CSharp-Stack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp_Stack
+{
+    class Stack<T>
+    {
+        T data;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
